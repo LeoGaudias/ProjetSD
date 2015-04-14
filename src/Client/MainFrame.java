5737a32635f43cba3d19f859978de7e17eaf74ea
Léo <leogaudias.pro@outlook.com>
2015-04-14 10:01:26 +0200

    ajout d'OpenGL

--- src/Client/MainFrame.java
+++ src/Client/MainFrame.java
@@ -7,13 +7,16 @@ import javax.swing.JFrame;
 
 public class MainFrame extends JFrame
 {
+	public Maps m;
+	
 	public MainFrame()
 	{
 		super();
+		m = new Maps();
 		this.setTitle("Project");
 		this.getContentPane().setPreferredSize(new Dimension(200,200));
+		this.getContentPane().add(m.canvas);
 		
-		//canvas.addGLEventListener();
 		
 	}
 }
--- src/Client/Maps.java
+++ src/Client/Maps.java
@@ -1,11 +1,42 @@
 package Client;
 
-public class Maps /*implements GLEventListener*/ {
+import com.jogamp.opengl.GLAutoDrawable;
+import com.jogamp.opengl.GLEventListener;
+import com.jogamp.opengl.awt.GLCanvas;
+
+public class Maps implements GLEventListener {
+	
+	public GLCanvas canvas;
 	
 	public Maps()
 	{
-		//GLCanvas canvas = new GLCanvas();
-		//canvas.addGLEventListener(new Article1());
+		canvas = new GLCanvas();
+		canvas.addGLEventListener(this);
+	}
+
+	@Override
+	public void display(GLAutoDrawable arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose(GLAutoDrawable arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void init(GLAutoDrawable arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void reshape(GLAutoDrawable arg0, int arg1, int arg2, int arg3,
+			int arg4) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 	

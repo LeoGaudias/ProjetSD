6e8343b4b1c5e71731b3d14e40cbd36ae40646fd
NiffleurOr16 <tori-0133@hotmail.fr>
2015-05-07 14:22:07 +0200

    calculdesetinationfinale

--- src/Client/Homme.java
+++ src/Client/Homme.java
@@ -5,7 +5,8 @@ import java.util.ArrayList;
 public class Homme {
 
 	private ArrayList<Integer> adn;
-	
+	int longueur;
+	int largeur;
 	public Homme()
 	{
 		adn = new ArrayList<Integer>();
@@ -25,5 +26,41 @@ public class Homme {
 		{
 			adn.add(deplacementRandom());
 		}
+	}
+
+	Point calculDestinationFinale() {
+		Point p = new Point(0,this.largeur/2);
+		for(int i=0; i < adn.size(); i++) {
+			switch(adn.get(i)) {
+				case 0:
+					p.setLocation(p.x,p.y++);
+					break;
+				case 1:
+					p.setLocation(p.x++,p.y++);
+					break;
+				case 2:
+					p.setLocation(p.x++,p.y);
+					break;
+				case 3:
+					p.setLocation(p.x++,p.y--);
+					break;
+				case 4:
+					p.setLocation(p.x,p.y--);
+					break;
+				case 5:
+					p.setLocation(p.x--,p.y--);
+					break;
+				case 6:
+					p.setLocation(p.x--,p.y);
+					break;
+				case 7:
+					p.setLocation(p.x--,p.y++);
+					break;
+				default:
+					p.setLocation(p.x--,p.y--);
+					break;
+			}
+		}
+		return null;
 	}
 }
